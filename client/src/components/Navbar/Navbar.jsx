1c74bcccdf6a4343becac1da3df719a4baaabebc
MrDychi <dylanrchan@gmail.com>
2026-02-16 11:29:47 -0800

    added small ui improvements

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,8 @@
-import { useState, useEffect } from 'react'
-import DoubleSlider from '../DoubleSlider/DoubleSlider.jsx'
+import { useState, useEffect } from 'react';
+import DoubleSlider from '../DoubleSlider/DoubleSlider.jsx';
+import menuIcon from '../../assets/menu.png';
+import dashboardIcon from '../../assets/dashboard.png';
+import tableIcon from '../../assets/table.png';
 import './Navbar.css'
 
 const Navbar = ({displayPage, setDisplayPage, fullYearRange, yearFilterRange, setYearFilterRange, fullCompanyList, companyFilter, setCompanyFilter}) => {
@@ -30,14 +33,18 @@ const Navbar = ({displayPage, setDisplayPage, fullYearRange, yearFilterRange, se
     return (
         <>
             <div className={collapsed ? "navbar collapsed" : "navbar expanded"}>
-                <button className="navbar-toggle-btn" onClick={toggleCollapse}></button>
+                <button className="navbar-toggle-btn" onClick={toggleCollapse}>
+                    <img src={menuIcon} alt="menu button" />
+                </button>
                 <div className="navbar-content">
                     <div 
                         className={displayPage === "dashboard" ? "navigation selected" : "navigation unselected"}
                         onClick={() => {setDisplayPage("dashboard")}}
                     >
                         <div className="indicator unselected"></div>
-                        <div>Main Dashboard</div>
+                        <div>
+                            <img src={dashboardIcon} alt="dashboard icon"/> Main Dashboard
+                        </div>
                         <div className={displayPage === "dashboard" ? "indicator selected" : "indicator unselected"}></div>
                     </div>
                     <div 
@@ -45,13 +52,14 @@ const Navbar = ({displayPage, setDisplayPage, fullYearRange, yearFilterRange, se
                         onClick={() => {setDisplayPage("table")}}
                     >
                         <div className="indicator unselected"></div>
-                        <div>Data Table</div>
+                        <div>
+                            <img src={tableIcon} alt="table icon" /> Data Table
+                        </div>
                         <div className={displayPage === "table" ? "indicator selected" : "indicator unselected"}></div>
                     </div>
                     <div className="nav-filter-container">
                         <div className="year-filter-container">
-                            <div>Filter by year range: (inclusive)</div>
-                            <div>{yearFilterRange[0]}-{yearFilterRange[1]}</div>
+                            <div className="filter-label">Filter by year range: (inclusive)</div>
                             <DoubleSlider 
                                 minValue={fullYearRange[0]} 
                                 maxValue={fullYearRange[1]} 
@@ -59,11 +67,13 @@ const Navbar = ({displayPage, setDisplayPage, fullYearRange, yearFilterRange, se
                                 onChange={setYearFilterRange}
                             >
                             </DoubleSlider>
+                            <div class="year-range-display">{yearFilterRange[0]}  -  {yearFilterRange[1]}</div>
                         </div>
-                        <div>Filter by company name:</div>
+                        <div className="filter-label">Filter by company name:</div>
                         <div className="company-filter-util-btns">
                             <button className="company-filter-util-btn" onClick={selectAllCompanies}>Select All</button>
                             <button className="company-filter-util-btn" onClick={deselectAllCompanies}>Reset</button>
+                            <div className="blankSpacer"></div>
                         </div>
                         <div className="company-filter-container">
                             {fullCompanyList.map((company) => {
